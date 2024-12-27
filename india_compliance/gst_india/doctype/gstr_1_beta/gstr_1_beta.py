a2872a02f7f32ffb59104770f7ceb8e753c2d24a
tinadn <tina@8848digital.com>
2024-12-27 19:01:18 +0530

    fix: remove unwanted changes

--- india_compliance/gst_india/doctype/gstr_1_beta/gstr_1_beta.py
+++ india_compliance/gst_india/doctype/gstr_1_beta/gstr_1_beta.py
@@ -124,6 +124,7 @@ class GSTR1Beta(Document):
         """
         Try to generate GSTR-1 data. Wrapper for generating GSTR-1 data
         """
+        
         filters = frappe._dict(
             company=self.company,
             company_gstin=self.company_gstin,
