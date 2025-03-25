6d2d5d05df6fc8f918419b526190d47e9a8affd1
anandk-spyke-o1 <anandkumar@8848digital.com>
2025-03-25 13:13:03 +0530

    fix: use correct update syntax for PostgreSQL in BOE patch

--- india_compliance/patches/v15/multiple_pi_in_boe.py
+++ india_compliance/patches/v15/multiple_pi_in_boe.py
@@ -2,14 +2,22 @@ import frappe
 
 
 def execute():
-    boe = frappe.qb.DocType("Bill of Entry", alias="boe")
-    boe_item = frappe.qb.DocType("Bill of Entry Item", alias="boe_item")
+    boe_item = frappe.qb.DocType("Bill of Entry Item")
+    boe = frappe.qb.DocType("Bill of Entry")
 
-    # link BOE Item to it's purchase invoice
-    (
-        frappe.qb.update(boe_item)
-        .join(boe)
-        .on(boe_item.parent == boe.name)
-        .set(boe_item.purchase_invoice, boe.purchase_invoice)
-        .run(as_dict=True)
-    )
+    if frappe.db.db_type == "postgres":
+        (
+            frappe.qb.update(boe_item)
+            .set(boe_item.purchase_invoice, boe.purchase_invoice)
+            .from_(boe)
+            .where(boe_item.parent == boe.name)
+            .run()
+        )
+    else:
+        (
+            frappe.qb.update(boe_item)
+            .join(boe)
+            .on(boe_item.parent == boe.name)
+            .set(boe_item.purchase_invoice, boe.purchase_invoice)
+            .run(as_dict=True)
+        )
