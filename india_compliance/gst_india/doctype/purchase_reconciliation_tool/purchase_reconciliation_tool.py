b1fbe038a21233456065d1cbb31315ec96770b2c
Barathpalanisamy <barath@8848digital.com>
2025-03-27 18:17:01 +0530

    Purchase Reconciliation Tool with additional export columns

--- india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.py
+++ india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.py
@@ -9,6 +9,9 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.query_builder.functions import IfNull
 from frappe.utils import add_to_date, cint, now_datetime
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+    get_accounting_dimensions,
+)
 
 from india_compliance.gst_india.api_classes.taxpayer_base import (
     TaxpayerBaseAPI,
@@ -963,6 +966,18 @@ class BuildExcel:
         ]
 
     def get_invoice_columns(self):
+        self.dimension_fields = ["project", "cost_center"] + get_accounting_dimensions()
+        dimension_columns = [
+            {
+                "label": frappe.unscrub(dimension),
+                "fieldname": dimension,
+                "data_format": {
+                    "horizontal": "left",
+                },
+            }
+            for dimension in self.dimension_fields
+        ]
+
         self.pr_columns = [
             {
                 "label": "Bill No",
@@ -1267,6 +1282,21 @@ class BuildExcel:
                 "data_format": {"horizontal": "left"},
                 "header_format": {
                     "width": 11,
+                },
+            },
+           *dimension_columns,
+            {
+                "label": "Inward Supply Name",
+                "fieldname": "inward_supply_name",
+                "data_format": {
+                    "horizontal": "left",
+                },
+            },
+            {
+                "label": "Purchase Document Name",
+                "fieldname": "purchase_invoice_name",
+                "data_format": {
+                    "horizontal": "left",
                 },
             },
             {
