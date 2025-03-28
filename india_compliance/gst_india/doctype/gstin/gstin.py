dcb6e91716b6c94a8cff48724c1c78beaeafaf12
Barathpalanisamy <barath@8848digital.com>
2025-03-28 17:16:17 +0530

    fix: check if settings are enabled for transporter id check

--- india_compliance/gst_india/doctype/gstin/gstin.py
+++ india_compliance/gst_india/doctype/gstin/gstin.py
@@ -240,6 +240,13 @@ def validate_gst_transporter_id(transporter_id):
         return
 
     gstin = None
+    settings = frappe.get_cached_doc("GST Settings")
+    if (
+        not settings.validate_gstin_status
+        or not is_api_enabled(settings)
+        or settings.sandbox_mode
+    ):
+        return
 
     # Check if GSTIN doc exists
     if frappe.db.exists("GSTIN", transporter_id):
