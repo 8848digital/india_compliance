c80b8fe4ac9d41f7bd60eac82aeeb12de56bb665
rakesh8848 <rakesh@8848digital.com>
2025-11-12 15:45:28 +0530

    refactor: use reference to qb.DocType for item

--- india_compliance/gst_india/doctype/gst_hsn_code/gst_hsn_code.py
+++ india_compliance/gst_india/doctype/gst_hsn_code/gst_hsn_code.py
@@ -75,7 +75,7 @@ def _update_item_modified_timestamp(item_names, timestamp=None):
         frappe.qb.update(item)
         .set(item.modified, timestamp or frappe.utils.now())
         .set(item.modified_by, frappe.session.user)
-        .where(frappe.qb.DocType("Item").name.isin(item_names))
+        .where(item.name.isin(item_names))
     ).run()
 
 def _add_comment_to_items(item_names, hsn_code, timestamp=None):
