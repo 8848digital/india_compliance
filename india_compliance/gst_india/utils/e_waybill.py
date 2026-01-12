d7c185b9c8d0eebcad08c213c6b852c3b60f2d76
ljain112 <ljain112@gmail.com>
2026-01-12 13:14:13 +0530

    chore: added docstrings for permission check
    (cherry picked from commit e9233fd4444cf5c5328adb1d4bd41bbfc555d993)

--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -56,7 +56,8 @@ from india_compliance.utils.change_log_utils import create_change_log_comment
 
 
 @frappe.whitelist()
-def generate_e_waybill(*, doctype, docname, values=None, force: bool = False):
+def generate_e_waybill_json(doctype: str, docnames, values=None):
+    """Permission check not required as load_doc checks permissions."""
     docnames = frappe.parse_json(docnames) if docnames.startswith("[") else [docnames]
     ewb_data = {
         "version": "1.0.0621",
@@ -137,7 +138,8 @@ def generate_e_waybills(doctype, docnames, force=False):
 
 
 @frappe.whitelist()
-def generate_e_waybill(*, doctype, docname, values=None, force=False):
+def generate_e_waybill(*, doctype, docname, values=None, force: bool = False):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     if values:
         update_transaction(doc, frappe.parse_json(values))
@@ -290,6 +292,7 @@ def log_and_process_e_waybill_generation(doc, result, *, with_irn=False):
 
 @frappe.whitelist()
 def cancel_e_waybill(*, doctype, docname, values):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "cancel")
     values = frappe.parse_json(values)
     _cancel_e_waybill(doc, values)
@@ -351,6 +354,7 @@ def log_and_process_e_waybill_cancellation(doc, values, result):
 
 @frappe.whitelist()
 def update_vehicle_info(*, doctype, docname, values):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
 
     old_values = {
@@ -479,6 +483,7 @@ def _bulk_update_transporter_in_docs(doctype, docnames, values):
 
 @frappe.whitelist()
 def update_transporter(*, doctype, docname, values):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     old_transporter_id = doc.gst_transporter_id
 
@@ -530,6 +535,7 @@ def update_transporter(*, doctype, docname, values):
 
 @frappe.whitelist()
 def extend_validity(*, doctype, docname, values, scheduled: bool = False):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     values = frappe.parse_json(values)
 
@@ -624,6 +630,7 @@ def validate_data_before_schedule(doc, values):
 
 @frappe.whitelist()
 def schedule_ewaybill_for_extension(doctype, docname, values, scheduled_time):
+    """Permission check not required as load_doc checks permissions."""
     values = frappe.parse_json(values)
     if not values:
         return
@@ -684,6 +691,7 @@ def generate_pending_e_waybills():
 def fetch_e_waybill_data(
     *, doctype, docname, attach: bool = False, force: bool = False
 ):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "write" if attach else "print")
     log = frappe.get_doc("e-Waybill Log", doc.ewaybill)
     if not log.is_latest_data or force:
@@ -714,6 +722,7 @@ def _fetch_e_waybill_data(doc, log):
 
 @frappe.whitelist()
 def find_matching_e_waybill(*, doctype, docname, e_waybill_date):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
 
     response = EWaybillAPI.create(doc).get_e_waybills_by_date(
@@ -747,6 +756,7 @@ def find_matching_e_waybill(*, doctype, docname, e_waybill_date):
 
 @frappe.whitelist()
 def mark_e_waybill_as_generated(doctype, docname, values):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     values = frappe.parse_json(values)
     result = {
@@ -763,6 +773,7 @@ def mark_e_waybill_as_generated(doctype, docname, values):
 
 @frappe.whitelist()
 def mark_e_waybill_as_cancelled(doctype, docname, values):
+    """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "cancel")
     values = frappe.parse_json(values)
     result = frappe._dict(
