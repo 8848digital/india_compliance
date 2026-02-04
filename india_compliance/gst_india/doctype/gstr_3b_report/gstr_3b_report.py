bb63fc4704b8d0b6572479553b9516942427e74f
ljain112 <ljain112@gmail.com>
2026-02-04 17:28:55 +0530

    fix(security): replace raw SQL with query builder in GSTR 3B Report
    - Refactored get_outward_items() to use frappe.qb query builder
    - Refactored get_missing_field_invoices() to use frappe.qb query builder
    - Eliminated SQL injection vulnerabilities from f-string usage
    
    Fixes semgrep rule: frappe-sql-format-injection
    
    (cherry picked from commit 64f3ed244f6fa1cb0e5ae2d5549d3905e0ed0503)

--- india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
+++ india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
@@ -751,18 +751,17 @@ class GSTR3BReport(Document):
                 else invoice.supplier_gstin
             )
 
-            query = (
+            docnames = (
                 frappe.qb.from_(invoice)
                 .select(invoice.name)
                 .where(invoice.docstatus == 1)
                 .where(invoice.is_opening == "No")
+                .where(invoice.posting_date.between(self.from_date, self.to_date))
                 .where(invoice.company == self.company)
                 .where(invoice.place_of_supply.isnull())
                 .where(invoice.company_gstin != IfNull(party_gstin, ""))
                 .where(invoice.gst_category != "Overseas")
-            )
-
-            docnames = self.apply_itc_period_filter(query, invoice).run(as_dict=True)
+            ).run(as_dict=True)
 
             for d in docnames:
                 missing_field_invoices.append(d.name)
