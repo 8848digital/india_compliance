fa8b907cfe409fff0b4224f0abda09545c1d26a6
ljain112 <ljain112@gmail.com>
2026-02-05 13:16:28 +0530

    chore: added type hints to whitelisted functions
    (cherry picked from commit 27d9b65bbd632eb38cd079daf6fb223bdcac7428)

--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -62,7 +62,9 @@ from india_compliance.utils.change_log_utils import create_change_log_comment
 
 
 @frappe.whitelist()
-def generate_e_waybill_json(doctype: str, docnames, values=None):
+def generate_e_waybill_json(
+    doctype: str, docnames: str | list, values: str | dict | None = None
+):
     """Permission check not required as load_doc checks permissions."""
     docnames = frappe.parse_json(docnames) if docnames.startswith("[") else [docnames]
     ewb_data = {
@@ -88,7 +90,9 @@ def generate_e_waybill_json(doctype: str, docnames, values=None):
 
 
 @frappe.whitelist()
-def bulk_update_transporter_in_docs(doctype, docnames, values):
+def bulk_update_transporter_in_docs(
+    doctype: str, docnames: str | list, values: str | dict
+):
     frappe.has_permission(doctype, "submit", throw=True)
 
     docnames = frappe.parse_json(docnames) if docnames.startswith("[") else [docnames]
@@ -98,7 +102,7 @@ def bulk_update_transporter_in_docs(doctype, docnames, values):
 
 
 @frappe.whitelist()
-def enqueue_bulk_e_waybill_generation(doctype, docnames):
+def enqueue_bulk_e_waybill_generation(doctype: str, docnames: str | list):
     """
     Enqueue bulk generation of e-Waybill for the given documents.
     """
@@ -146,7 +150,9 @@ def generate_e_waybills(doctype, docnames, force=False):
 
 
 @frappe.whitelist()
-def generate_e_waybill(*, doctype, docname, values=None, force: bool = False):
+def generate_e_waybill(
+    *, doctype: str, docname: str, values: str | dict | None = None, force: bool = False
+):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     if values:
@@ -362,7 +368,7 @@ def log_and_process_e_waybill_generation(doc, result, *, with_irn=False):
 
 
 @frappe.whitelist()
-def cancel_e_waybill(*, doctype, docname, values):
+def cancel_e_waybill(*, doctype: str, docname: str, values: str | dict):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "cancel")
     values = frappe.parse_json(values)
@@ -424,7 +430,7 @@ def log_and_process_e_waybill_cancellation(doc, values, result):
 
 
 @frappe.whitelist()
-def update_vehicle_info(*, doctype, docname, values):
+def update_vehicle_info(*, doctype: str, docname: str, values: str | dict):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
 
@@ -553,7 +559,7 @@ def _bulk_update_transporter_in_docs(doctype, docnames, values):
 
 
 @frappe.whitelist()
-def update_transporter(*, doctype, docname, values):
+def update_transporter(*, doctype: str, docname: str, values: str | dict):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     old_transporter_id = doc.gst_transporter_id
@@ -607,7 +613,9 @@ def update_transporter(*, doctype, docname, values):
     return send_updated_doc(doc)
 
 @frappe.whitelist()
-def extend_validity(*, doctype, docname, values, scheduled: bool = False):
+def extend_validity(
+    *, doctype: str, docname: str, values: str | dict, scheduled: bool = False
+):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     values = frappe.parse_json(values)
@@ -702,7 +710,9 @@ def validate_data_before_schedule(doc, values):
 
 
 @frappe.whitelist()
-def schedule_ewaybill_for_extension(doctype, docname, values, scheduled_time):
+def schedule_ewaybill_for_extension(
+    doctype: str, docname: str, values: str | dict, scheduled_time: str
+):
     """Permission check not required as load_doc checks permissions."""
     values = frappe.parse_json(values)
     if not values:
@@ -762,7 +772,7 @@ def generate_pending_e_waybills():
 
 @frappe.whitelist()
 def fetch_e_waybill_data(
-    *, doctype, docname, attach: bool = False, force: bool = False
+    *, doctype: str, docname: str, attach: bool = False, force: bool = False
 ):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "write" if attach else "print")
@@ -794,7 +804,7 @@ def _fetch_e_waybill_data(doc, log):
 
 
 @frappe.whitelist()
-def find_matching_e_waybill(*, doctype, docname, e_waybill_date):
+def find_matching_e_waybill(*, doctype: str, docname: str, e_waybill_date: str):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
 
@@ -828,7 +838,7 @@ def find_matching_e_waybill(*, doctype, docname, e_waybill_date):
 
 
 @frappe.whitelist()
-def mark_e_waybill_as_generated(doctype, docname, values):
+def mark_e_waybill_as_generated(doctype: str, docname: str, values: str | dict):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "submit")
     values = frappe.parse_json(values)
@@ -845,7 +855,7 @@ def mark_e_waybill_as_generated(doctype, docname, values):
 
 
 @frappe.whitelist()
-def mark_e_waybill_as_cancelled(doctype, docname, values):
+def mark_e_waybill_as_cancelled(doctype: str, docname: str, values: str | dict):
     """Permission check not required as load_doc checks permissions."""
     doc = load_doc(doctype, docname, "cancel")
     values = frappe.parse_json(values)
@@ -915,8 +925,8 @@ def get_pdf_filename(e_waybill_number):
 
 @frappe.whitelist()
 def get_valid_and_invalid_e_waybill_log(
-    doctype,
-    docs,
+    doctype: str,
+    docs: str | list,
 ):
     """
     - Validate e-Waybill Log
@@ -1187,7 +1197,7 @@ def get_address_map(doc):
 
 
 @frappe.whitelist()
-def get_source_destination_address(doctype, docname, address_type):
+def get_source_destination_address(doctype: str, docname: str, address_type: str):
     # using load_doc for onload trigger required for stock entry.
     doc = load_doc(doctype, docname)
     address_map = get_billing_shipping_address_map(doc)
