8304d6a63cb1c5363e9a03382366241f4920f2f4
Karm Soni <karmdsoni8159@gmail.com>
2026-02-11 18:20:59 +0530

    fix: handle e-waybill status updates only for Sales Invoices
    (cherry picked from commit b48ab9c82476c6e9d8d16a9e90886a6881715ee4)

--- india_compliance/gst_india/utils/__init__.py
+++ india_compliance/gst_india/utils/__init__.py
@@ -1255,4 +1255,7 @@ def set_ewaybill_status(
     commit=False,
     notify=True,
 ):
+    if doc.doctype != "Sales Invoice":
+        return
+
     doc.db_set("e_waybill_status", status, commit=commit, notify=notify)
