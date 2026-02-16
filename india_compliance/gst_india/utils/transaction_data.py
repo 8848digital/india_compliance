404e4e2082ef5d036e8118b95a087adae1893b35
Karm Soni <karmdsoni8159@gmail.com>
2026-02-16 12:00:41 +0530

    refactor: introduce VALID_HSN_LENGTHS constant for consistent HSN length validation
    (cherry picked from commit 0e4fe29c1537bbced6b0c51afb8025154a92cfe6)

--- india_compliance/gst_india/utils/transaction_data.py
+++ india_compliance/gst_india/utils/transaction_data.py
@@ -11,6 +11,7 @@ from india_compliance.gst_india.constants import (
     GST_TAX_TYPES,
     SUBCONTRACTING_DOCTYPES,
     TAXABLE_GST_TREATMENTS,
+    VALID_HSN_LENGTHS,
 )
 from india_compliance.gst_india.constants.e_waybill import (
     TRANSPORT_MODES,
@@ -308,7 +309,7 @@ class GSTTransactionData:
 
         _validate_hsn_codes(
             self.doc,
-            valid_hsn_length=[4, 6, 8],
+            valid_hsn_length=VALID_HSN_LENGTHS,
             message=_(
                 "Since HSN/SAC Code is mandatory for generating e-Waybill/e-Invoices.<br>"
             ),
