472f8be06640cb7fbbf94e42a1f56f3fd0a6f548
Navin-S-R <navin@aerele.in>
2026-03-05 10:06:12 +0530

    fix(asset depreciation): calculate WDV depreciation using depreciable value

--- india_compliance/income_tax_india/overrides/asset_depreciation_schedule.py
+++ india_compliance/income_tax_india/overrides/asset_depreciation_schedule.py
@@ -75,9 +75,9 @@ def get_wdv_or_dd_depr_amount(asset_depreciation_schedule, row_idx):
 
     if asset_depreciation_schedule.fb_row.frequency_of_depreciation == 12:
         if schedule_date < start_date_of_next_fiscal_year:
-            depreciation_amount = flt(
-                asset_depreciation_schedule.asset_doc.gross_purchase_amount
-            ) * (flt(rate_of_depreciation) / 100)
+            depreciation_amount = flt(depreciable_value) * (
+                flt(rate_of_depreciation) / 100
+            )
         else:
             depreciation_amount = flt(
                 asset_depreciation_schedule.yearly_opening_wdv
@@ -110,9 +110,7 @@ def get_wdv_or_dd_depr_amount(asset_depreciation_schedule, row_idx):
 
         if schedule_date < start_date_of_next_fiscal_year:
             depreciation_amount = (
-                flt(asset_depreciation_schedule.asset_doc.gross_purchase_amount)
-                * (flt(rate_of_depreciation) / 100)
-                * fraction
+                flt(depreciable_value) * (flt(rate_of_depreciation) / 100) * fraction
             )
         else:
             depreciation_amount = (
