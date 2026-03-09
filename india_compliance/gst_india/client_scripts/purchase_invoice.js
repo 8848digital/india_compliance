065f72b8e979ab6f7c0bfa3cb45b31d4e4428949
Smit Vora <smitvora203@gmail.com>
2026-03-09 16:47:09 +0530

    fix(ux): mark the field as read-only after 3b is filed in purchase docs
    (cherry picked from commit db9d27d126c085cf32203a924e887810e3459368)

--- india_compliance/gst_india/client_scripts/purchase_invoice.js
+++ india_compliance/gst_india/client_scripts/purchase_invoice.js
@@ -16,12 +16,11 @@ frappe.ui.form.on(DOCTYPE, {
                 },
             };
         });
+
+        india_compliance.setup_itc_claim_period_query(frm);
     },
 
-    onload(frm) {
-        toggle_reverse_charge(frm);
-        frm.set_df_property("itc_claim_period", "ignore_validation", 1);
-    },
+    onload: toggle_reverse_charge,
 
     gst_category(frm) {
         validate_gst_hsn_code(frm);
@@ -42,12 +41,13 @@ frappe.ui.form.on(DOCTYPE, {
                 message: __("Supplier Address is required to create e-Waybill"),
                 indicator: "yellow",
             },
-            10
+            10,
         );
     },
 
     refresh(frm) {
         india_compliance.set_reconciliation_status(frm, "bill_no");
+        india_compliance.set_itc_claim_period_status(frm);
         if (gst_settings.enable_e_waybill && gst_settings.enable_e_waybill_from_pi)
             show_sandbox_mode_indicator();
 
@@ -66,7 +66,7 @@ frappe.ui.form.on(DOCTYPE, {
                     frm: frm,
                 });
             },
-            __("Create")
+            __("Create"),
         );
     },
 
@@ -86,7 +86,7 @@ frappe.ui.form.on(DOCTYPE, {
                     frm.doc.name,
                     frm._inward_supply.name,
                     "Purchase Invoice",
-                    false
+                    false,
                 );
             };
             frappe.set_route("Form", frm._inward_supply.source_doc);
@@ -112,7 +112,7 @@ function toggle_reverse_charge(frm) {
     else if (
         frm.doc.items.length > 0 &&
         frm.doc.items.some(
-            item => item.gst_hsn_code && !item.gst_hsn_code.startsWith("99")
+            item => item.gst_hsn_code && !item.gst_hsn_code.startsWith("99"),
         )
     )
 
