c219fb51e9a833b9af86e5090d14390a2cc538ce
Smit Vora <smitvora203@gmail.com>
2026-03-09 16:18:39 +0530

    fix: ignore validation for auto-complete fields
    (cherry picked from commit c07cdaa4af71d4588f4a3f25bb2140597ee1cf29)

--- india_compliance/gst_india/doctype/bill_of_entry/bill_of_entry.js
+++ india_compliance/gst_india/doctype/bill_of_entry/bill_of_entry.js
@@ -2,9 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Bill of Entry", {
+    setup(frm) {
+        frm.set_query("itc_claim_period", () => {
+            return {
+                query: "india_compliance.gst_india.utils.itc_claim.get_itc_period_options",
+                params: {
+                    company_gstin: frm.doc.company_gstin,
+                    posting_date: frm.doc.posting_date,
+                },
+            };
+        });
+    },
+
     onload(frm) {
         frm.fields_dict.items.grid.cannot_add_rows = true;
         frm.bill_of_entry_controller = new BillOfEntryController(frm);
+
+        frm.set_df_property("itc_claim_period", "ignore_validation", 1);
     },
 
     refresh(frm) {
