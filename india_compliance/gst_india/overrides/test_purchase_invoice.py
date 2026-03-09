1578998897d747f1e80572686505644064aea4af
Smit Vora <smitvora203@gmail.com>
2026-03-09 16:57:56 +0530

    test: fix failing tests because of recent changes
    (cherry picked from commit 017f104e9b08570e458ca7475c8dde4ef2f1fc06)

--- india_compliance/gst_india/overrides/test_purchase_invoice.py
+++ india_compliance/gst_india/overrides/test_purchase_invoice.py
@@ -295,7 +295,7 @@ class TestPurchaseInvoice(FrappeTestCase):
         self.assertRaisesRegex(
             frappe.exceptions.ValidationError,
             re.compile(
-                r"Cannot change ITC Claim Period from .* to .*\. GSTR-3B already filed\."
+                r"Cannot change ITC Claim Period from .* to .*\. GSTR-3B already filed for .*\."
             ),
             pinv.save,
         )
@@ -307,7 +307,7 @@ class TestPurchaseInvoice(FrappeTestCase):
         self.assertRaisesRegex(
             frappe.exceptions.ValidationError,
             re.compile(
-                r"Cannot change ITC Claim Period from .* to .*\. GSTR-3B already filed\."
+                r"Cannot change ITC Claim Period from .* to .*\. GSTR-3B already filed for .*\."
             ),
             pinv.save,
         )
