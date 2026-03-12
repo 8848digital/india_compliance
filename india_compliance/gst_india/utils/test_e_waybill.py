d4a4ac4718ce4636a97c23a019e4e2c9809c052e
Karm Soni <karmdsoni8159@gmail.com>
2026-03-12 12:55:37 +0530

    fix: change TestEWaybillThreshold to inherit from FrappeTestCase

--- india_compliance/gst_india/utils/test_e_waybill.py
+++ india_compliance/gst_india/utils/test_e_waybill.py
@@ -1698,7 +1698,7 @@ def with_intrastate_config(config_rows):
     return decorator
 
 
-class TestEWaybillThreshold(IntegrationTestCase):
+class TestEWaybillThreshold(FrappeTestCase):
     """Tests for state-wise e-Waybill threshold configuration (PR #3968)"""
 
     @classmethod
