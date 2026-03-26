01b57411f6c97c6e91b9b54cb3d1b00835ccb625
ljain112 <ljain112@gmail.com>
2026-03-26 16:57:02 +0530

    fix: change onchange logic for early return
    (cherry picked from commit afa30bb85100036f0cceb21b1ed57ca127a83096)

--- india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.js
+++ india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.js
@@ -1121,11 +1121,15 @@ class ImportDialog {
                     const period = this.dialog.get_value("period");
                     if (!period) return;
 
-                    this.date_range = this.dialog.get_value("date_range");
-                    if (period !== "Custom") {
-                        const { message } = await this.frm._call("get_date_range", {
-                            period,
-                        });
+                    if (period === "Custom") {
+                        this.date_range = this.dialog.get_value("date_range");
+                        this.fetch_import_history();
+                        return;
+                    }
+
+                    const { message } = await this.frm._call("get_date_range", {
+                        period,
+                    });
 
                         if (message) this.date_range = message;
                     }
