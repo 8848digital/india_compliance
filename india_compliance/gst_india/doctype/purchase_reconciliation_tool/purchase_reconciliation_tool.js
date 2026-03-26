aa92ac2b05d80b38c996cf6701a30600ee9b3746
ljain112 <ljain112@gmail.com>
2026-03-26 19:00:31 +0530

    refactor: refactor as per suggestion
    (cherry picked from commit b848e7ed8a264042b46ea1167eeff3a96286e5d5)

--- india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.js
+++ india_compliance/gst_india/doctype/purchase_reconciliation_tool/purchase_reconciliation_tool.js
@@ -1121,15 +1121,11 @@ class ImportDialog {
                     const period = this.dialog.get_value("period");
                     if (!period) return;
 
-                    if (period === "Custom") {
-                        this.date_range = this.dialog.get_value("date_range");
-                        this.fetch_import_history();
-                        return;
-                    }
-
-                    const { message } = await this.frm._call("get_date_range", {
-                        period,
-                    });
+                    this.date_range = this.dialog.get_value("date_range");
+                    if (period !== "Custom") {
+                        const { message } = await this.frm._call("get_date_range", {
+                            period,
+                        });
 
                         if (message) this.date_range = message;
                     }
