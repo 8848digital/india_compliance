84b76215af3ae484a41790f4bbf4039a68701e51
ljain112 <ljain112@gmail.com>
2026-03-30 15:09:57 +0530

    fix: enhance e-waybill handling for SEZ transactions and add related tests
    (cherry picked from commit 815c21fd22989b59bf2b3eb4073f430716b76659)

--- india_compliance/gst_india/constants/e_waybill.py
+++ india_compliance/gst_india/constants/e_waybill.py
@@ -41,6 +41,8 @@ ADDRESS_GSTIN_FIELD_MAP = {
 }
 PERMITTED_DOCTYPES = list(ADDRESS_FIELDS.keys())
 
+BUYING_DOCTYPES = {doctype for doctype, address in ADDRESS_FIELDS.items() if address is buying_address}
+
 CANCEL_REASON_CODES = {
     "Duplicate": "1",
     "Order Cancelled": "2",
