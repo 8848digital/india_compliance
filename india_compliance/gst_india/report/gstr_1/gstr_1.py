24fbb613f400a405f84823962360dcacd42ddba6
Karm Soni <karmdsoni8159@gmail.com>
2026-04-06 19:16:21 +0530

    fix: update get_rate_and_tax_details to include item index for better tracking

--- india_compliance/gst_india/report/gstr_1/gstr_1.py
+++ india_compliance/gst_india/report/gstr_1/gstr_1.py
@@ -1692,8 +1692,8 @@ def get_b2b_json(res, gstin):
                 continue
             inv_item["itms"] = []
 
-            for item in invoice:
-                inv_item["itms"].append(get_rate_and_tax_details(item, gstin))
+            for num, item in enumerate(invoice, 1):
+                inv_item["itms"].append(get_rate_and_tax_details(item, gstin, num))
 
             inv.append(inv_item)
 
@@ -1795,12 +1795,12 @@ def get_b2cl_json(res, gstin):
 
         b2cl_item, inv = {"pos": "%02d" % int(pos.split("-")[0]), "inv": []}, []
 
-        for row in res[pos]:
+        for num, row in enumerate(res[pos], 1):
             inv_item = get_basic_invoice_detail(row)
             if row.get("sale_from_bonded_wh"):
                 inv_item["inv_typ"] = "CBW"
 
-            inv_item["itms"] = [get_rate_and_tax_details(row, gstin)]
+            inv_item["itms"] = [get_rate_and_tax_details(row, gstin, num)]
 
             inv.append(inv_item)
 
@@ -1873,8 +1873,8 @@ def get_cdnr_reg_json(res, gstin):
             }
 
             inv_item["itms"] = []
-            for item in invoice:
-                inv_item["itms"].append(get_rate_and_tax_details(item, gstin))
+            for num, item in enumerate(invoice, 1):
+                inv_item["itms"].append(get_rate_and_tax_details(item, gstin, num))
 
             inv.append(inv_item)
 
@@ -1908,8 +1908,8 @@ def get_cdnr_unreg_json(res, gstin):
             )
 
         inv_item["itms"] = []
-        for item in items:
-            inv_item["itms"].append(get_rate_and_tax_details(item, gstin))
+        for num, item in enumerate(items, 1):
+            inv_item["itms"].append(get_rate_and_tax_details(item, gstin, num))
 
         out.append(inv_item)
 
@@ -2053,7 +2053,7 @@ def get_shipping_bill_details(row):
     }
 
 
-def get_rate_and_tax_details(row, gstin):
+def get_rate_and_tax_details(row, gstin, num):
     itm_det = {
         "txval": flt(row["taxable_value"], 2),
         "rt": row["rate"],
@@ -2061,7 +2061,6 @@ def get_rate_and_tax_details(row, gstin):
     }
 
     # calculate rate
-    num = 1 if not row["rate"] else "%d%02d" % (row["rate"], 1)
     rate = row.get("rate") or 0
 
     # calculate tax amount added
