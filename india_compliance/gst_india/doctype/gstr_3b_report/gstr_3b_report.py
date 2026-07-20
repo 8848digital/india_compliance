55576becd21d6d2647c7c6d46e2a8d1195b69aca
ljain112 <ljain112@gmail.com>
2026-07-20 17:20:21 +0530

    fix: ensure GSTR-3B report generation notification is sent after commit
    (cherry picked from commit 24678cc62453af4a3fe685362e08b9e2ee8639ad)

--- india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
+++ india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
@@ -213,7 +213,13 @@ class GSTR3BReport(Document):
             raise e
 
         finally:
-            frappe.publish_realtime("gstr3b_report_generation", doctype=self.doctype, docname=self.name)
+            # after_commit so the client never reloads before the row is visible
+            frappe.publish_realtime(
+                "gstr3b_report_generation",
+                doctype=self.doctype,
+                docname=self.name,
+                after_commit=True,
+            )
 
     def _get_gstr1_filters(self):
         """Filters for GSTR1Invoices (Sales Invoice data)."""
